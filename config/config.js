cd6d9e59e4a8595d41f9de61d7490ba9aa1a3588
Suleyman Melikoglu <suleymanmelikoglu@gmail.com>
2014-04-12 12:03:19 +0300

    initial node project

---
+++ config/config.js
@@ -0,0 +1,30 @@
+
+/*!
+ * Module dependencies.
+ */
+
+var path = require('path')
+var rootPath = path.resolve(__dirname + '../..')
+
+/**
+ * Expose config
+ */
+
+module.exports = {
+  development: {
+    root: rootPath,
+    db: 'mongodb://localhost/vine_fetcher_dev'
+  },
+  test: {
+    root: rootPath,
+    db: 'mongodb://localhost/vine_fetcher_test'
+  },
+  staging: {
+    root: rootPath,
+    db: process.env.MONGOHQ_URL
+  },
+  production: {
+    root: rootPath,
+    db: process.env.MONGOHQ_URL
+  }
+}
