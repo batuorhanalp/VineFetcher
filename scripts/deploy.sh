2a7cc553da1d0ba052d2180d84717cec5758fb61
Suleyman Melikoglu <suleymanmelikoglu@gmail.com>
2014-04-15 14:44:16 +0300

    forgot to pull changes on deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,2 +1,2 @@
-ssh $1 'cd ~/node/VineFetcher; forever stop 0;forever start /usr/local/bin/npm start'
+ssh $1 'cd ~/node/VineFetcher; git pull origin master; forever stop 0;forever start /usr/local/bin/npm start'
 echo "hell yeah"
