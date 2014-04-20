b8412a8b92824689f96c3727f81bc392839bd053
batuorhanalp <batu@orhanalp.com>
2014-04-20 14:02:50 +0300

    Routes changed to relative

--- config/routes.js
+++ config/routes.js
@@ -7,7 +7,7 @@ var basePath = 'http://report.karbonat.com/bolmalzemos';
 var mongoose = require('mongoose');
 var passportOptions = {
   failureFlash: 'Invalid email or password.',
-  failureRedirect: '/login'
+  failureRedirect: 'login'
 };
 
 // controllers
@@ -20,7 +20,7 @@ var home = require('home'),
 var requiresLogin = function(req, res, next) {
   if (req.isAuthenticated()) return next();
   if (req.method == "GET") req.session.returnTo = req.originalUrl;
-  res.redirect(basePath + '/admin/login');
+  res.redirect('admin/login');
 };
 
 
@@ -31,30 +31,30 @@ module.exports = function (app, passport) {
 
   app.namespace(basePath, function() {
     // public pages
-    app.get(basePath, home.index);
-    app.get(basePath + '/videos', home.videos);
+    app.get('' + basePath, home.index);
+    app.get('videos', home.videos);
 
     // auth pages
-    app.get(basePath + '/admin/login', admin.login);
-    app.post(basePath + '/admin/login', passport.authenticate(
+    app.get('admin/login', admin.login);
+    app.post('admin/login', passport.authenticate(
           'local', 
           { 
-            successRedirect: basePath + '/admin', 
-            failureRedired: basePath + '/admin/login' 
+            successRedirect: 'admin', 
+            failureRedired: 'admin/login' 
           }));
-    app.get(basePath + '/admin/logout', requiresLogin, admin.logout);
-    app.get(basePath + '/admin/create', requiresLogin, admin.createAdmin);
-    app.post(basePath + '/admin/create', requiresLogin, admin.createAdminPost);
+    app.get('admin/logout', requiresLogin, admin.logout);
+    app.get('admin/create', requiresLogin, admin.createAdmin);
+    app.post('admin/create', requiresLogin, admin.createAdminPost);
 
     // admin pages
-    app.get(basePath + '/admin', requiresLogin, admin.index);
-    app.post(basePath + '/admin/video/approve', requiresLogin, admin.approve);
-    app.post(basePath + '/admin/video/delete', requiresLogin, admin.disapprove);
-    app.get(basePath + '/admin/video/ids', requiresLogin, admin.approved);
+    app.get('admin', requiresLogin, admin.index);
+    app.post('admin/video/approve', requiresLogin, admin.approve);
+    app.post('admin/video/delete', requiresLogin, admin.disapprove);
+    app.get('admin/video/ids', requiresLogin, admin.approved);
 
     // user pages
-    app.get(basePath + '/users', requiresLogin, users.index);
-    app.post(basePath + '/users/delete', requiresLogin, users.deleteUser);
+    app.get('users', requiresLogin, users.index);
+    app.post('users/delete', requiresLogin, users.deleteUser);
   });
 
 };
